75c65703bb5da7c0f7936c5ef853415311b7725d
Bamidelejulius12 <babatundejulius911@gmail.com>
2026-01-02 20:02:56 +0100

    first commit before dvc

---
+++ code.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+
+## Adding new rows for v2
+# new_row_loc = {'Name': V2, 'Age':20, 'City':City1}
+# df.loc[len(df.index)] = new_row_loc
+
+## Adding new rows for v3
+# new_row_loc2 = {'Name': V3, 'Age':30, 'City':City1}
+# df.loc[len(df.index)] = new_row_loc2
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# save the dataframe to a csv file, including column names
+df.to_csv(file_path, index = False)
+print(f"CSV file saved to {file_path}")
